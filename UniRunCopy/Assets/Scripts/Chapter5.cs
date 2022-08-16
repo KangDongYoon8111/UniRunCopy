5547a578da178dec4e44f704e4caa3a0248e5017
강동윤 <davidkang8111@gmail.com>
2022-08-16 10:47:47 +0900

    20220816

--- UniRunCopy/Assets/Scripts/Chapter5.cs
+++ UniRunCopy/Assets/Scripts/Chapter5.cs
@@ -6,12 +6,16 @@ public class Chapter5 : MonoBehaviour
 {
     void Start()
     {
-        for (int i = 0; i < 2; i++)
+        int[] class1 = new int[5] { 10, 20, 30, 40, 50 };
+        class1 = new int[6] { 10, 20, 30, 40, 50, 60 };
+
+        int sum1 = 0;
+        foreach (int a in class1)
         {
-            for (int j = 0; j <= i; j++)
-            {
-                Debug.Log("*");
-            }
+            sum1 += a;
         }
+        Debug.Log($"{class1.Length}명 학생의 평균은 {sum1 / class1.Length}");
+
+        Debug.Log($"6명 학생의 평균은 {sum1 / 6}");
     }
 }
